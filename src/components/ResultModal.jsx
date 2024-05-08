3cac96ed38b2716092526f189be3b37bd443ca67
Larry Scroggins <larry.scroggins@gmail.com>
2024-05-08 16:05:02 -0700

    Start on refs and forwardRefs

---
+++ src/components/ResultModal.jsx
@@ -0,0 +1,14 @@
+import { forwardRef } from "react";
+
+const ResultModal = forwardRef(function ResultModal({result, targetTime}, ref) {
+    return <dialog ref={ref} className="result-modal">
+        <h2>You {result}</h2>
+        <p>The target time was <strong>{targetTime}</strong> seconds.</p>
+        <p>You stopped the time with <strong>x</strong> seconds left.</p>
+        <form method="dialog">
+            <button>Close</button>
+        </form>
+    </dialog>
+});
+
+export default ResultModal;
